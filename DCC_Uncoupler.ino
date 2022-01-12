8b787f5b36e4bc81c9d629049aa38002ef73c6be
SharpSharp <chris@copmanthorpe.org.uk>
2022-01-12 10:38:46 +0000

    removing the ConfigureDecoder() function
    The functionality in ConfigureDecoder() has been moved. The setting of addresses and pins is now done with at the time of object construction, and an shorthand format is used. The setting up of the output pins has been encapsulated into the DCCAccessoryAddress struct.

--- DCC_Uncoupler.ino
+++ DCC_Uncoupler.ino
@@ -24,46 +24,28 @@ typedef struct
  uint8_t arduinoPin;
  bool uncoupling;         // is the uncoupler switched on
  unsigned long offTime;   // time when output will be set to LOW
+
+ void setup()
+ {
+  pinMode(arduinoPin, OUTPUT);
+ }
 }
 DCCAccessoryAddress;
 
 // set number of DCC addresses
-DCCAccessoryAddress uncoupler[NUMBER_OF_UNCOUPLERS];
+DCCAccessoryAddress uncoupler[NUMBER_OF_UNCOUPLERS] = {
+  {23, 4},
+  {27, 5},
+  {30, 6},
+  {31, 7},
+  {24, 8},
+  {28, 9}
+};
 
 NmraDcc  Dcc;
 uint16_t lastAddr = 0xFFFF;
 uint8_t lastDirection = 0xFF;
 
-//
-// Decoder Init
-//
-void ConfigureDecoder()
-{
- // DCC addresses corresponding to arduino pins
- uncoupler[0].address = 23;
- uncoupler[0].arduinoPin = 4;
-
- uncoupler[1].address = 27;
- uncoupler[1].arduinoPin = 5;
-
- uncoupler[2].address = 30;
- uncoupler[2].arduinoPin = 6;
-
- uncoupler[3].address = 31;
- uncoupler[3].arduinoPin = 7;
-
- uncoupler[4].address = 24;
- uncoupler[4].arduinoPin = 8;
-
- uncoupler[5].address = 28;
- uncoupler[5].arduinoPin = 9;
-
- // set the pin for output
- for (int i = 0; i<NUMBER_OF_UNCOUPLERS; i++)
- {
-   pinMode(uncoupler[i].arduinoPin, OUTPUT);
- }
-}
 
 // This function is called whenever a normal DCC Turnout Packet is received
 void notifyDccAccTurnoutOutput(uint16_t Addr, uint8_t Direction, uint8_t OutputPower)
@@ -88,13 +70,14 @@ void setupDCCDecoder()
  // Call the main DCC Init function to enable the DCC Receiver
  Dcc.init(MAN_ID_DIY, 10, CV29_ACCESSORY_DECODER | CV29_OUTPUT_ADDRESS_MODE, 0);
 
- // Configure the Decoder
- ConfigureDecoder();
 }
 
 void setup()
 {
  setupDCCDecoder();
+ for (int i = 0; i<NUMBER_OF_UNCOUPLERS; i++) {
+  uncoupler[i].setup();
+ }
 }
 
 void loop()
